43226d7bae21a3217442420d96ec3554a32a154e
iamsid47 <siddheshkulthe43@gmail.com>
2023-01-22 21:31:12 +0530

    added class and functions

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 
 class Base {
-	
+	public:
+		int a;
+		float b;
+		double c;
+		
+		void fun1 (int x) {
+			a = x;
+		}	
+		
+		float fun1 (float y, float z) {
+			b = y + z;
+			cout << "The value of b is " << b << endl;
+			return b;
+		}
+		
+		double fun1 (float y) {
+			c = y * y;
+			cout << "The value of c is " << c << endl;
+			return c;
+		}
 };
-int main(int argc, char** argv) {
+int main() {
 	return 0;
 }
